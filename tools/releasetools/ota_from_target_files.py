88f7b11d1173d944256b8f7a560cafefcbbae736
XrTV <binder_z@mail.ru>
2017-08-27 05:48:02 +0400

    Octogen OS Logo
    Change-Id: Iac413a5cfb2b8ca5a9465d06f4876a7849a74c8c

--- tools/releasetools/ota_from_target_files.py
+++ tools/releasetools/ota_from_target_files.py
@@ -482,6 +482,20 @@ else if get_stage("%(bcb_dev)s") == "3/3" then
   if "selinux_fc" in OPTIONS.info_dict:
     WritePolicyConfig(OPTIONS.info_dict["selinux_fc"], output_zip)
 
+    model = GetBuildProp("ro.product.model", OPTIONS.info_dict)
+    build = GetBuildProp("ro.build.date", OPTIONS.info_dict)
+
+    script.Print("- by XrTV .-.-.-.-.-.-.-.-.-.- Android 8 .");
+    script.Print("**** **** *** **** **** *** *  *  **** ***");
+    script.Print("*  * *  *  *  *  * *    *   ** *  *  * *");
+    script.Print("*  * *     *  *  * * ** *** * **  *  * ***");
+    script.Print("*  * *  *  *  *  * *  * *   *  *  *  *   *");
+    script.Print("**** ****  *  **** **** *** *  *  **** ***");
+    script.Print("-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.");
+    script.Print(" Compiled: %s "%(build));
+    script.Print(" For: %s "%(model));
+    script.Print("-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.");
+
   recovery_mount_options = OPTIONS.info_dict.get("recovery_mount_options")
 
   script.ShowProgress(system_progress, 0)
